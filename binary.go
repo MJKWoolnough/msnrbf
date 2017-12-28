9b30895c6ca1f9bab67d278886f133526b6c03b3
Michael Woolnough <michael.woolnough@gmail.com>
2017-12-28 11:53:12 +0000

    added String methods to enums

--- binary.go
+++ binary.go
@@ -17,6 +17,29 @@ func (r *reader) ReadBinaryTypeEnumeration() binaryTypeEnumeration {
 	return binaryTypeEnumeration(r.ReadByte())
 }
 
+func (b binaryTypeEnumeration) String() string {
+	switch b {
+	case binaryTypePrimitive:
+		return "primitive"
+	case binaryTypeString:
+		return "string"
+	case binaryTypeObject:
+		return "object"
+	case binaryTypeSystemClass:
+		return "system class"
+	case binaryTypeClass:
+		return "type class"
+	case binaryTypeObjectArray:
+		return "object array"
+	case binaryTypeStringArray:
+		return "string array"
+	case binaryTypePrimitiveArray:
+		return "primitive array"
+	default:
+		return "unknown binary type"
+	}
+}
+
 type binaryArrayTypeEnumeration byte
 
 const (
@@ -30,4 +53,23 @@ const (
 
 func (r *reader) ReadBinaryArrayTypeEnumeration() binaryArrayTypeEnumeration {
 	return binaryArrayTypeEnumeration(r.ReadByte())
+}
+
+func (b binaryArrayTypeEnumeration) String() string {
+	switch b {
+	case binaryArrayTypeSingle:
+		return "single"
+	case binaryArrayTypeJagged:
+		return "jagged"
+	case binaryArrayTypeRectangular:
+		return "rectangular"
+	case binaryArrayTypeSingleOffset:
+		return "single offset"
+	case binaryArrayTypeJaggedOffset:
+		return "jagged offset"
+	case binaryArrayTypeRectangularOffset:
+		return "rectangular offset"
+	default:
+		return "unknown binary array type"
+	}
 }
--- primitive.go
+++ primitive.go
@@ -26,3 +26,44 @@ const (
 func (r *reader) ReadPrimitiveTypeEnum() primitiveTypeEnum {
 	return primitiveTypeEnum(r.ReadByte())
 }
+
+func (p primitiveTypeEnum) String() string {
+	switch p {
+	case primitiveTypeBoolean:
+		return "boolean"
+	case primitiveTypeByte:
+		return "byte"
+	case primitiveTypeChar:
+		return "char"
+	case primitiveTypeDecimal:
+		return "decimal"
+	case primitiveTypeDouble:
+		return "double"
+	case primitiveTypeInt16:
+		return "int16"
+	case primitiveTypeInt32:
+		return "int32"
+	case primitiveTypeInt64:
+		return "int64"
+	case primitiveTypeSByte:
+		return "int8"
+	case primitiveTypeSingle:
+		return "float32"
+	case primitiveTypeTimeSpan:
+		return "duration"
+	case primitiveTypeDateTime:
+		return "datetime"
+	case primitiveTypeUInt16:
+		return "uint16"
+	case primitiveTypeUInt32:
+		return "uint32"
+	case primitiveTypeUInt64:
+		return "uint64"
+	case primitiveTypeNull:
+		return "null"
+	case primitiveTypeString:
+		return "string"
+	default:
+		return "unknown primitive"
+	}
+}
