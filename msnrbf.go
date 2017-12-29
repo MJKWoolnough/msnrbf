ee8e581de6230c89182d136f82422cc0d35fe037
Michael Woolnough <michael.woolnough@gmail.com>
2017-12-29 17:54:18 +0000

    read first structure after header

--- msnrbf.go
+++ msnrbf.go
@@ -8,7 +8,8 @@ import (
 )
 
 type File struct {
-	RootID int32
+	serializedStreamHeader
+	library interface{}
 }
 
 func Open(ra io.ReaderAt) (*File, error) {
@@ -20,57 +21,41 @@ func Open(ra io.ReaderAt) (*File, error) {
 	if sh.MajorVersion != 1 || sh.MinorVersion != 0 {
 		return nil, ErrInvalidVersion
 	}
-	for {
-		typ := r.ReadRecordTypeEnumeration()
-		if r.Err != nil {
-			return nil, r.Err
-		}
-		switch typ {
-		case recordClassWithID:
-			r.ReadClassWithID()
-		case recordSystemClassWithMembers:
-			r.ReadSystemClassWithMembers()
-		case recordClassWithMembers:
-			r.ReadClassWithMembers()
-		case recordSystemClassWithMembersAndTypes:
-			r.ReadSystemClassWithMembersAndTypes()
-		case recordClassWithMembersAndTypes:
-			r.ReadClassWithMembersAndTypes()
-		case recordBinaryObjectString:
-			r.ReadBinaryObjectString()
-		case recordBinaryArray:
-			r.ReadBinaryArray()
-		case recordMemberPrimitiveTyped:
-			r.ReadMemberPrimitiveTyped()
-		case recordMemberReference:
-			r.ReadMemberReference()
-		case recordObjectNull:
-			r.ReadObjectNull()
-		case recordMessageEnd:
-			r.ReadMessageEnd()
-		case recordBinaryLibrary:
-			r.ReadBinaryLibrary()
-		case recordObjectNullMultiple256:
-			r.ReadObjectNullMultiple256()
-		case recordObjectNullMultiple:
-			r.ReadObjectNullMultiple()
-		case recordArraySinglePrimitive:
-			r.ReadArraySinglePrimitive()
-		case recordArraySingleObject:
-			r.ReadArraySingleObject()
-		case recordArraySingleString:
-			r.ReadArraySingleString()
-		case recordMethodCall:
-			r.ReadMethodCall()
-		case recordMethodReturn:
-			r.ReadMethodReturn()
-		default:
-			return nil, fmt.Errorf("unhandled type: %d", typ)
-		}
+	typ := r.ReadRecordTypeEnumeration()
+	if r.Err != nil {
+		return nil, r.Err
 	}
-	return &File{
-		RootID: sh.RootID,
-	}, nil
+	switch typ {
+	case recordClassWithID:
+		r.ReadClassWithID()
+	case recordSystemClassWithMembers:
+		r.ReadSystemClassWithMembers()
+	case recordClassWithMembers:
+		r.ReadClassWithMembers()
+	case recordSystemClassWithMembersAndTypes:
+		r.ReadSystemClassWithMembersAndTypes()
+	case recordClassWithMembersAndTypes:
+		r.ReadClassWithMembersAndTypes()
+	case recordBinaryObjectString:
+		r.ReadBinaryObjectString()
+	case recordBinaryArray:
+		r.ReadBinaryArray()
+	case recordBinaryLibrary:
+		r.ReadBinaryLibrary()
+	case recordArraySinglePrimitive:
+		r.ReadArraySinglePrimitive()
+	case recordArraySingleObject:
+		r.ReadArraySingleObject()
+	case recordArraySingleString:
+		r.ReadArraySingleString()
+	case recordMethodCall:
+		r.ReadMethodCall()
+	case recordMethodReturn:
+		r.ReadMethodReturn()
+	default:
+		return nil, fmt.Errorf("invalid type: %d", typ)
+	}
+	return &File{}, nil
 }
 
 var (
