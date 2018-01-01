6c5bbec5b7a07e3db5333b186bc0ed7b9e9dcb6c
Michael Woolnough <michael.woolnough@gmail.com>
2018-01-01 16:32:20 +0000

    stub'd

--- server.go
+++ server.go
@@ -1,13 +1,32 @@
 package msnrbf
 
-import "io"
+import (
+	"io"
+	"net/rpc"
+)
 
 type serverCodec struct {
 	conn io.ReadWriteCloser
 }
 
-func NewServerCodec(conn io.ReadWriteCloser) rpc.NewServerCodec {
+func NewServerCodec(conn io.ReadWriteCloser) rpc.ServerCodec {
 	return &serverCodec{
 		conn: conn,
 	}
+}
+
+func (s *serverCodec) ReadRequestHeader(r *rpc.Request) error {
+	return nil
+}
+
+func (s *serverCodec) ReadRequestBody(v interface{}) error {
+	return nil
+}
+
+func (s *serverCodec) WriteResponse(r *rpc.Response, v interface{}) error {
+	return nil
+}
+
+func (s *serverCodec) Close() error {
+	return nil
 }
